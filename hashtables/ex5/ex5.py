faf6e7115c945e66c962b9d76979a258ba3d36b1
andrewhpark25 <andrewhpark25@gmail.com>
2020-07-10 10:42:03 -0700

    MVP

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -8,7 +8,25 @@ def finder(files, queries):
     """
     # Your code here
 
+    cache = dict()
+    result = []
+    
+    for path in files:
+        item = path.split('/')[-1]
+        # append path if item in cache
+        if item in cache:
+            cache[item].append(path)
+        # create a list with path if not 
+        else:
+            cache[item] = [path]
+      # check if each query in cache
+    for query in queries:
+        if query in cache:
+            # add to result if in cache
+           result.extend(cache[query])
+                
     return result
+
 
 
 if __name__ == "__main__":
